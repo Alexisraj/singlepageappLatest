fd0388a1587b63ecbc083f24ac3af16d419f323f
Alexisraj <80027669+Alexisraj@users.noreply.github.com>
2021-04-10 21:17:51 +0800

    format fix

--- src/body/body.js
+++ src/body/body.js
@@ -1,33 +1,45 @@
-import './body.css'
-import HomePage from '../section/homepage';
-import Contact from '../section/contact';
-import AboutUs from '../section/aboutus';
+import "./body.css";
+import HomePage from "../section/homepage";
+import Contact from "../section/contact";
+import AboutUs from "../section/aboutus";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-  
-function Body(){
-    return (
-        <div className="body" > 
-        <Router>
-            <div className="menu">
-                <Link to="/">Home</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/about">About US</Link>
-            </div>
-            <div className="content">
-            <Switch>
-                <Route exact path="/"><HomePage /></Route>
-                <Route exact path="/contact"><Contact /></Route>
-                <Route exact path="/about"><AboutUs /></Route>
-            </Switch>
-            </div>
-        </Router>
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
+
+function Body() {
+  return (
+    <div className="body">
+      <Router>
+        <div className="menu">
+          <NavLink to="/" activeStyle={{ fontWeight: "bold" }}>
+            Home
+          </NavLink>
+          <NavLink to="/contact" activeStyle={{ fontWeight: "bold" }}>
+            Contact
+          </NavLink>
+          <NavLink to="/about" activeStyle={{ fontWeight: "bold" }}>
+            About Us
+          </NavLink>
         </div>
-    );
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+            <Route exact path="/about">
+              <AboutUs />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default Body;
